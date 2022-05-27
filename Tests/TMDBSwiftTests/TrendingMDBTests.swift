e5b9868fb4f6b65f23eb47e8303e9bd53e15ca1d
Dylan Pearce <djpearce13@gmail.com>
2022-05-27 14:35:08 -0400

    Add TrendingMDB tests (#75)

---
+++ Tests/TMDBSwiftTests/TrendingMDBTests.swift
@@ -0,0 +1,67 @@
+//
+//  TrendingMDBTests.swift
+//  
+//
+//  Created by Dylan Pearce on 5/27/22.
+//
+
+@testable import TMDBSwift
+import XCTest
+
+final class TrendingMDBTests: XCTestCase {
+
+    let expecationTimeout: TimeInterval = 50
+
+    override func setUp() {
+        super.setUp()
+        TMDBConfig.apikey = "8a7a49369d1af6a70ec5a6787bbfcf79"
+    }
+
+    func testTVTrendingDay() {
+        var data: [TVMDB]!
+        let expectation = self.expectation(description: "Wait for data to load.")
+
+        TrendingMDB.trending(mediaType: .tv, timeWindow: .day) { _, _, tvData in
+            data = tvData
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: expecationTimeout, handler: nil)
+        XCTAssertNotNil(data)
+    }
+
+    func testTVTrendingWeek() {
+        var data: [TVMDB]!
+        let expectation = self.expectation(description: "Wait for data to load.")
+
+        TrendingMDB.trending(mediaType: .tv, timeWindow: .week) { _, _, tvData in
+            data = tvData
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: expecationTimeout, handler: nil)
+        XCTAssertNotNil(data)
+    }
+
+    func testMovieTrendingDay() {
+        var data: [MovieMDB]!
+        let expectation = self.expectation(description: "Wait for data to load.")
+
+        TrendingMDB.trending(mediaType: .movie, timeWindow: .day) { _, movieData, _ in
+            data = movieData
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: expecationTimeout, handler: nil)
+        XCTAssertNotNil(data)
+    }
+
+    func testMovieTrendingWeek() {
+        var data: [MovieMDB]!
+        let expectation = self.expectation(description: "Wait for data to load.")
+
+        TrendingMDB.trending(mediaType: .movie, timeWindow: .week) { _, movieData, _ in
+            data = movieData
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: expecationTimeout, handler: nil)
+        XCTAssertNotNil(data)
+    }
+}
