fb727690c5e1a798b38b2353a6606a31fe880e77
Dylan Pearce <djpearce13@gmail.com>
2022-05-27 16:34:13 -0400

    Add NetworkMDB tests

---
+++ Tests/TMDBSwiftTests/NetworksMDBTests.swift
@@ -0,0 +1,35 @@
+//
+//  NetworksMDBTests.swift
+//  
+//
+//  Created by Dylan Pearce on 5/27/22.
+//
+
+@testable import TMDBSwift
+import XCTest
+
+final class NetworksMDBTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        TMDBConfig.apikey = "8a7a49369d1af6a70ec5a6787bbfcf79"
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+
+    func testNetworks() {
+        var data: NetworksMDB!
+        let expectation = self.expectation(description: "Wait for data to load.")
+
+        NetworksMDB.networks(networkId: 1) { _, network in
+            data = network
+            expectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 50, handler: nil)
+        XCTAssertEqual(data?.name, "Fuji TV")
+        XCTAssertEqual(data?.id, 1.0)
+    }
+}
