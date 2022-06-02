d20a9aa8f4f6c455a13179b1edc76c2bdefa2798
Dylan Pearce <djpearce13@gmail.com>
2022-06-01 20:20:22 -0400

    Add DocC workflow (#88)

--- Package.swift
+++ Package.swift
@@ -16,6 +16,7 @@ let package = Package(
             targets: ["TMDBSwift"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
     ],
     targets: [
         .target(
