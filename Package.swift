2bf9d9a17abb3c65c85ac8be74bcc71e0ab2e158
Dylan Pearce <djpearce13@gmail.com>
2022-06-17 20:50:49 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,7 +8,7 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .tvOS(.v10),
-        .macOS(.v10_15),
+        .macOS(.v12),
     ],
     products: [
         .library(
